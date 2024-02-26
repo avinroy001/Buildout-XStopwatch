29fbe55fdcb78ee351d78154b8c8eb5ed986dd3b
Avinash <avin.roy001@gmail.com>
2024-02-27 03:37:28 +0530

    complete commit

--- xstopwatch/src/App.jsx
+++ xstopwatch/src/App.jsx
@@ -1,23 +1,59 @@
-import { useState } from 'react'
+import { useState ,useEffect } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sec, setSec] = useState(0);
+  const [min,setMin]=useState(0);
+  const [start,setStart]=useState(false);
+  // const [reset,setReset]=useState(false);
+  // const [stop,setStop]=useState(true);
+  const [buttonText,setButtonText]=useState('Start')
 
-  setInterval(()=>{
-    
+  const startStop=()=>{
+    if(start==true){
+      setStart(false);
+    }else{
+      setStart(true);
+    }
+  }
+  
+  var timer;
+  useEffect(()=>{
+    // var timer;
+    if(start==true){
+      setButtonText('Stop');
+    timer=setInterval(()=>{
+      setSec(sec+1);
+
+      if(sec==59){
+        setMin(min+1);
+        setSec(0);
+      }
+    },1000)
+    }else{
+      ()=>clearInterval(timer);
+      setButtonText('Start');
+      // setSec(0);
+      // setMin(0);
+    }
+    return ()=>clearInterval(timer);
   })
+
+  const resett=()=>{
+    setStart(false);
+    clearInterval(timer);
+    setSec(0);
+    setMin(0);
+  }
 
   return (
     <>
       <h1>Stopwatch</h1>
+      <h3>Time: {min}:{sec<10?"0"+sec : sec}</h3>
       <br/>
-      <br/>
-      <h3>Time:</h3>
-      <br/>
-      <button>Start</button> 
-      <button>Reset</button>  
+      <button onClick={()=>{startStop()}}>{buttonText}</button> 
+      <button onClick={()=>{resett()}}>Reset</button>  
     </>
   )
 }
