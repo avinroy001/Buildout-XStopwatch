522c554e12b3fbe507fc0e8446636c0b8ef79821
Avinash <avin.roy001@gmail.com>
2024-02-27 18:02:00 +0530

    change way

--- xstopwatch/src/App.jsx
+++ xstopwatch/src/App.jsx
@@ -4,51 +4,42 @@ import './App.css'
 
 function App() {
   const [sec, setSec] = useState(0);
-  const [min,setMin]=useState(0);
   const [start,setStart]=useState(false);
-  // const [reset,setReset]=useState(false);
-  // const [stop,setStop]=useState(true);
-  // const [buttonText,setButtonText]=useState('Start')
-
   const startStop=()=>{
-    if(start==true){
-      setStart(false);
-    }else{
-      setStart(true);
-    }
+      setStart(!start);
   }
-  // console.log(start)
-  // var timer;
+  
   useEffect(()=>{
-    var timer;
-    if(start==true){
+    let timer;
+    if(start){
     timer=setInterval(()=>{
-      setSec(sec+1);
-
-      if(sec==59){
-        setMin(min+1);
-        setSec(0);
-      }
+      setSec(p=>p+1); 
     },1000)
     }else{
-      ()=>clearInterval(timer);
+      clearInterval(timer);
     }
     return ()=>clearInterval(timer);
-  },[min, sec, start])
+  },[start])
 
   const resett=()=>{
     setStart(false);
     setSec(0);
-    setMin(0);
   }
+
+  const formatTime=(time)=>{
+      const minutes=Math.floor(time/60);
+      const seconds=Math.floor(time%60);
+
+      return `${minutes}:${seconds<10?'0':''}${seconds}`;
+  };
 
   return (
     <>
       <h1>Stopwatch</h1>
-      <h3>Time: {min}:{sec<10?"0"+sec : sec}</h3>
+      <p>{formatTime(sec)}</p>
       <br/>
-      <button onClick={()=>{startStop()}}>{start ? "Stop" : "Start"}</button> 
-      <button onClick={()=>{resett()}}>Reset</button>  
+      <button onClick={startStop}>{start ? "Stop" : "Start"}</button> 
+      <button onClick={resett}>Reset</button>  
     </>
   )
 }
